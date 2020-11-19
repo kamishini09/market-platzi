316af83df84509ad1e924ce4bf243da1def50560
Hernando <hernandolopez09@gmail.com>
2020-11-18 19:32:17 -0500

    primero

---
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -0,0 +1,51 @@
+package com.platzi.market.domain.service;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product){
+        return  productRepository.save(product);
+    }
+
+    public boolean delete(int productId){
+        /*Si el producto existe ejecuta el map y elimina el producto y devuelve el true
+        * de lo contrario retorna un false*/
+        return getProduct(productId).map(product -> {
+            productRepository.delete(productId);
+            return true;
+        }).orElse(false);
+
+        /*----otra forma----
+        * ambas formas son propias de los Optional*/
+        /*
+        if (getProduct(productId).isPresent()){
+            productRepository.delete(productId);
+            return true;
+        }else {
+            return false;
+        }
+        */
+    }
+}
---
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -0,0 +1,63 @@
+package com.platzi.market.persistence;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
+import com.platzi.market.persistence.crud.ProductoCrudRepository;
+import com.platzi.market.persistence.entity.Producto;
+import com.platzi.market.persistence.mapper.ProductoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository implements ProductRepository {
+    @Autowired
+    private ProductoCrudRepository productoCrudRepository;
+
+    /*Inyeccion de dependencias : contenedeor de inversion de control de spring Autowired
+    * solo se puede hacer si el objeto o el atributo debe de ser un componenete de spring */
+    @Autowired
+    private ProductoMapper mapper;
+
+    @Override
+    public List<Product>getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos=  productoCrudRepository.finByCantidadStockLessThanAndEstado(quantity, true);
+        return  productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+    @Override
+    public void delete(int productId) {
+        productoCrudRepository.deleteById(productId);
+    }
+
+
+
+
+
+}
